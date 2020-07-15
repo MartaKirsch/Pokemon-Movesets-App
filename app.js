e4b6884a06470dc4ee11053d521d75b0cf0876b8
MartaKirsch <martakirsch00@gmail.com>
2020-07-15 12:05:39 +0200

    first commit, startpoint

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+//create app
+const app = express();
+
+//listen to port 3000
+app.listen(3000);
+
+//static files
+app.use('/public', express.static('public'));
+
+//set view engine
+app.set('view engine', 'ejs');
+
+//get requests
+app.get('/', (req, res)=>{
+  res.render('index');
+});
